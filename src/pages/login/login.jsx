66d5628fdcd7769d29e93c9a50fd02eebc913481
minhtq <konosuba2324@gmail.com>
2024-06-11 15:30:05 +0700

    setup router, pages, assets

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useContext, useEffect } from 'react'
+import './styles.scss'
+
+const Login = () => {
+    return (
+        <div>login page</div>
+    )
+}
+
+export default Login
