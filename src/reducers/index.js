447f9b1b4cba2a5c7a4c1574798177d9e8f9fbeb
Duchai232 <Duchai232>
2024-06-12 11:46:16 +0700

    reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import commonReducer from "./commonReducer";
 import { combineReducers } from "redux";
+import navigationReducer from "./navigationReducer";
 
 const rootReducer = combineReducers({
   common: commonReducer,
+  navigations: navigationReducer,
 });
 
 export default rootReducer;
