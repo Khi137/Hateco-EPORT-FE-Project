218c190459da3d28f6caa86f6d5a7df11219a07d
minhtq <konosuba2324@gmail.com>
2024-06-15 11:49:39 +0700

    add css for Mcheckbox, MButton

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -139,10 +139,7 @@ class Login extends Component {
         const checkboxDataSource = {
             label: "Ghi nhớ mật khẩu",
             value: formData.remember,
-            dataSource: {
-                span: 12,
-                value: formData.remember,
-            }
+            className: `${formData.remember && "m-checkbox_checked"}`,
         };
 
         const inputForm = [
@@ -183,7 +180,7 @@ class Login extends Component {
                         {inputForm.map((item, key) => this.renderInputField(item, key))}
 
                         <Col className="form_item space_margin">
-                            <Mcheckbox dataSource={checkboxDataSource} onClick={() => this.handleCheckboxChange(!formData.remember)} />
+                            <Mcheckbox onChangeValue={(returnValue) => this.handleCheckboxChange(returnValue?.checked)} dataSource={checkboxDataSource} />
                         </Col>
 
                         <Col className="form_item">
