b0104ec1d00945a4262bd5403afc1b7491376771
Duchai232 <Duchai232>
2024-06-18 17:54:42 +0700

    layout dashboard

--- src/router/MainRoutes.jsx
+++ src/router/MainRoutes.jsx
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import Tongquan from "../Tongquan";
 import Login from "../views/login/login";
+import Dashboard from "../components/Dashboard/Dashboard";
 
 class MainRoutes extends Component {
   render() {
-    return <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Tongquan />} />
-    </Routes>;
+    return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Tongquan />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    );
   }
 }
 
