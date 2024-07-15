c2af122376f8d5b0c0d0bf9177061a9a48e18b4f
Duchai232 <Duchai232>
2024-07-15 14:40:33 +0700

    conflict

---
+++ src/views/task/emptyPickup.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class EmptyPickup extends Component {
+  render() {
+    return <div>EmptyPickup</div>;
+  }
+}
+
+export default EmptyPickup;
---
+++ src/views/task/stuffingOrder.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class StuffingOrder extends Component {
+  render() {
+    return <div>StuffingOrder</div>;
+  }
+}
+
+export default StuffingOrder;
